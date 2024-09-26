0d2f6dd798183ae61315e9235b04b50ccd9c4000
charite-uwatwembi <c.uwatwembi@alustudent.com>
2024-09-26 16:08:01 +0200

    REACT PRO

--- reactproject/ReactPro/src/App.jsx
+++ reactproject/ReactPro/src/App.jsx
@@ -1,19 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div className="App">
-        REACT PRO
-      </div>
-     
-    </>
-  )
+  const text = "REACT PRO";
+  return <div className="App">{ text }</div>;
 }
-
 export default App
